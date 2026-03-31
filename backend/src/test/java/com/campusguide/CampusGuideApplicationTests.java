ee72e533781882b3697a899595bcb83b4bab5936
김은찬 <ksadh10@naver.com>
2026-03-31 13:45:35 +0900

    project init

---
+++ backend/src/test/java/com/campusguide/CampusGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
