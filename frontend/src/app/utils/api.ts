3ec07af4de1cc804102953c63cb65b38e9ab15f4
jsh00315 <jsh00315@naver.com>
2026-06-06 16:06:02 +0900

    로그인 안됌

--- frontend/src/app/utils/api.ts
+++ frontend/src/app/utils/api.ts
@@ -1,4 +1,5 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+console.log('BASE_URL:', BASE_URL);
 
 export const getToken = () => localStorage.getItem('token');
 
